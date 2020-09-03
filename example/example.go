34443d1df03de504fabef9f1e707ad9741751c53
Jeff <jeffxf@users.noreply.github.com>
2020-09-03 04:09:29 +0000

    Updated comments in example

--- example/example.go
+++ example/example.go
@@ -9,7 +9,7 @@ import (
 	"github.com/jeffxf/nojwt"
 )
 
-// tokenData describes the data within our token
+// TokenData describes the data within our token
 type TokenData struct {
 	Username      string `json:"username"`
 	UserID        int    `json:"userid"`
@@ -19,12 +19,12 @@ type TokenData struct {
 func main() {
 	// Generate a new random key pair
 	publicKey, privateKey, err := ed25519.GenerateKey(rand.Reader)
-	// You can create a key pair externally and provide it instead which is what
-	// you probably want to do. Otherwise, a new key will be genereated
+	// You can create a key pair externally and provide it instead which is
+	// what you probably want to do. Otherwise, a new key will be genereated
 	// everytime your app is restarted which will invalidate all of the
 	// previously signed tokens
 
-	//Set the fields of the token we want to create
+	// Set the fields of the token we want to create
 	tokenData := TokenData{
 		Username:      "jeffxf",
 		UserID:        501,
@@ -39,9 +39,9 @@ func main() {
 	}
 
 	// Once the token has been sent to a client and returned back to you, any of
-	// your services that have the public key can verify the signature of a
+	// your services that have the public key can verify that the signature of a
 	// token is valid and read the data in one shot via the Decode function.
-	// Let's extract the data into a new instance of our Claims struct
+	// Let's extract the data into a new instance of our TokenData struct
 	var decodedTokenData TokenData
 	err = nojwt.Decode(publicKey, signedToken, &decodedTokenData)
 	if err != nil {
